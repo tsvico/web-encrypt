24eb5f63d6cf591b26c0ec7b93388ccc871a6051
gwj <gwjwith@outlook.com>
2021-02-04 09:40:32 +0800

    修改PWA

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import './pure.min.css';
 import reportWebVitals from './reportWebVitals';
+import { register } from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+register()
